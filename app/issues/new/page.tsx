c744ec9188ef38cb6d038d04e3692267f9fe1d17
yamchips <wflunch@gmail.com>
2025-05-05 20:18:09 +0100

    Organize code and move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -30,6 +30,20 @@ const NewIssuePage = () => {
   const router = useRouter();
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      if (axios.isAxiosError(error)) {
+        setError(error.message);
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    }
+  });
   return (
     <div className="max-w-xl ">
       {error && (
@@ -37,24 +51,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            if (axios.isAxiosError(error)) {
-              console.log(error);
-              setError(error.message);
-            } else {
-              setError("An unexpected error occurred.");
-            }
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
